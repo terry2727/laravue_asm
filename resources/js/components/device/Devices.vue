9a25d592c91864e669e4754cc7a37a43c8bbcc4a
terry27 <terrynguyen2727@gmail.com>
2021-01-21 05:41:51 +0700

    Add vue bootstrap datatable

--- resources/js/components/device/Devices.vue
+++ resources/js/components/device/Devices.vue
@@ -1,4 +1,4 @@
-
+<!--
  <template>
   <div>
     <h1>Devices</h1>
@@ -67,6 +67,94 @@ export default {
           this.devices.splice(i, 1);
         });
     },
+  },
+};
+</script>
+
+-->
+
+<template>
+  <div id="app">
+    <h1>Devices</h1>
+    <div class="row">
+      <div class="col-md-10"></div>
+      <div class="col-md-2">
+        <router-link :to="{ name: 'add_device' }" class="btn btn-primary"
+          >Add Device</router-link
+        >
+      </div>
+    </div>
+    <br />
+    <vue-bootstrap4-table :rows="devices" :columns="columns" :config="config">
+    </vue-bootstrap4-table>
+  </div>
+</template>
+
+<script>
+import VueBootstrap4Table from "vue-bootstrap4-table";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      devices: [],
+      columns: [
+        {
+          label: "Name",
+          name: "name",
+          filter: {
+            type: "simple",
+            placeholder: "Device name",
+          },
+          sort: true,
+        },
+        {
+          label: "Model",
+          name: "model",
+          filter: {
+            type: "simple",
+            placeholder: "Enter model",
+          },
+          sort: true,
+        },
+        {
+          label: "Producer",
+          name: "producer",
+          sort: true,
+        },
+        {
+          label: "Functions",
+          name: "functions",
+        },
+        {
+          label: "Actions",
+          name: "actions",
+        },
+      ],
+      config: {
+        checkbox_rows: false,
+        rows_selectable: true,
+        card_title: "List of devices",
+      },
+    };
+  },
+  created() {
+    this.axios.get("http://localhost:8000/api/devices").then((response) => {
+      this.devices = response.data;
+    });
+  },
+  methods: {
+    deleteDevice(id) {
+      this.axios
+        .delete(`http://localhost:8000/api/device/delete/${id}`)
+        .then((response) => {
+          let i = this.devices.map((item) => item.id).indexOf(id); // find index of your object
+          this.devices.splice(i, 1);
+        });
+    },
+  },
+  components: {
+    VueBootstrap4Table,
   },
 };
 </script>
