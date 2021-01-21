e20648ad6af60340591d6732b5352ca22c85cb07
terry27 <terrynguyen2727@gmail.com>
2021-01-21 15:43:29 +0700

    Add api for User(not AppUser)

--- app/Http/Controllers/API/AppUserController.php
+++ app/Http/Controllers/API/AppUserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\AppUser;
 use Validator;
+use Illuminate\Support\Facades\Auth;
 
 class AppUserController extends Controller
 {
