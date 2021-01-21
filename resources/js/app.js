3f68428ca056d3a02d2395728ea90d6d79bfeb91
Nam Nguyen <namnm1991@gmail.com>
2021-01-21 13:23:25 +0700

    Update flow to show deserts by Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,19 +6,19 @@ import App from './App.vue';
 import VueRouter from 'vue-router';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
-import {routes} from './routes';
+import { routes } from './routes';
 import Vuetify from 'vuetify/lib'
 
-Vue.use(Vuetify)
- 
+// Vue.use(Vuetify)
+
 Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
- 
+
 const router = new VueRouter({
     mode: 'history',
     routes: routes
 });
- 
+
 const app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
